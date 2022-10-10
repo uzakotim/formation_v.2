baec85196dc3d8cc00181e53ab50d1d1ba0c4acc
uzakotim <timauzakov@gmail.com>
2022-10-10 17:45:49 +0200

    Edge Detector working

--- cameras_reader/include/cameras_reader/CameraNode.hpp
+++ cameras_reader/include/cameras_reader/CameraNode.hpp
@@ -1,32 +1,30 @@
 #ifndef MY_CLASS
 #define MY_CLASS
-#include <nodelet/nodelet.h>
+/* includes //{ */
 
+/* each ROS nodelet must have these */
 #include <ros/ros.h>
 #include <ros/package.h>
-#include "cameras_reader/CameraNode.hpp"
-#include <ros/ros.h>
-#include <std_msgs/String.h>
 #include <nodelet/nodelet.h>
-#include <pluginlib/class_list_macros.h>
-#include <stdio.h>
-#include <sensor_msgs/image_encodings.h>
 #include <sensor_msgs/Image.h>
+#include <string.h>
+#include <std_msgs/String.h>
 
 namespace cameras_reader
 {
     class CameraNode : public nodelet::Nodelet
     {
         public:
-            CameraNode(){}
+            virtual void onInit();
         private:
+            
             ros::Publisher pub;
             ros::Subscriber sub;
             /* Ros parameters */
             std::string _uav_name_;
             std::string topic = "/rgbd_down/color/image_raw";
             
-            virtual void onInit();
+            
             void callback(const sensor_msgs::ImageConstPtr& msg);
     };
 
---
+++ example_ros_vision/CHANGELOG.rst
@@ -0,0 +1,8 @@
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+Changelog for package vision_example
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+1.0.0 (2021-03-18)
+------------------
+* Major release
+* Contributors: Matej Petrlik, Matouš Vrba, Tomáš Báča
