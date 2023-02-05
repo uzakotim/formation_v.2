5861e0558d4645938f9d0352a3f51570f662b0c1
uzakotim <timauzakov@gmail.com>
2023-02-05 15:04:54 +0100

    correction for mrs singularity

--- tmux/start.sh
+++ tmux/start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Absolute path to this script. /home/user/bin/foo.sh
-SCRIPT=$(readlink -f $0)
+#SCRIPT=$(readlink -f $0)
 # Absolute path this script is in. /home/user/bin
-SCRIPTPATH=`dirname $SCRIPT`
-cd "$SCRIPTPATH"
+#SCRIPTPATH=`dirname $SCRIPT`
+#cd "$SCRIPTPATH"
 
 # start tmuxinator
 tmuxinator start -p ./session.yml
